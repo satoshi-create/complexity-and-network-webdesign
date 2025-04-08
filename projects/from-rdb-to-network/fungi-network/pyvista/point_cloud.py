8ca13b83aa201d1a1ff241d1a5ecbae4d3de52ff
Jasmine <jasmine.s.jy.thompson@gmail.com>
2025-04-08 18:01:54 +0100

    Managed to get a running prototype of streamlit containing the real fungal network

--- projects/from-rdb-to-network/fungi-network/pyvista/point_cloud.py
+++ projects/from-rdb-to-network/fungi-network/pyvista/point_cloud.py
@@ -1,16 +1,29 @@
 import numpy as np
-import pyvista
+import pyvista as pv
 import streamlit as st
+import stpyvista
+
+## Initialize a plotter object
+plotter = pv.Plotter(pv.Plotter(window_size=[400,400]))
 
 rng = np.random.default_rng(seed=0)
 point_cloud = rng.random((100, 3))
-pdata = pyvista.PolyData(point_cloud)
+pdata = pv.PolyData(point_cloud)
 pdata['orig_sphere'] = np.arange(100)
 
 # create many spheres from the point cloud
-sphere = pyvista.Sphere(radius=0.02, phi_resolution=10, theta_resolution=10)
+sphere = pv.Sphere(radius=0.02, phi_resolution=10, theta_resolution=10)
 pc = pdata.glyph(scale=False, geom=sphere, orient=False)
-pc.plot(cmap='Reds')
+#pc.plot(cmap='Reds')
+
+plotter.view_isometric()
+plotter.set_background="white"
+
+plotter.add_mesh(sphere)
+plotter.add_mesh(pc, cmap="Reds")
 
 # doesn't integrate with streamlit so no point in drawing
 #st.draw(pc)
+
+## Send to streamlit
+stpyvista.stpyvista(plotter)
--- projects/from-rdb-to-network/fungi-network/pyvista/real_fungal_network.py
+++ projects/from-rdb-to-network/fungi-network/pyvista/real_fungal_network.py
@@ -21,7 +21,7 @@ edge_weights = [G[u][v]["weight"] * 1.5 for u, v in G.edges]
 pos = nx.spring_layout(G, seed=42, weight=None, dim=3)
 
 coords = []
-for value in pos.values():
+for key, value in pos.items():
     coords.append([value[0], value[1], value[2]])
 coords = np.array(coords, dtype="float32")
 
@@ -31,7 +31,19 @@ print("Min Coord:", coords.min())
 pdata = pyvista.PolyData(coords)
 pdata['orig_sphere'] = np.arange(len(G.nodes))
 
+print(pdata)
+
 # create many spheres from the point cloud
+plotter = pyvista.Plotter()
 sphere = pyvista.Sphere(radius=0.02, phi_resolution=10, theta_resolution=10)
+spline = pyvista.Tube(radius=0.01)
 pc = pdata.glyph(scale=False, geom=sphere, orient=False)
-pc.plot(cmap='Reds')
+pc.plot(cmap="Reds")
+pc2 = pdata.glyph(scale=False, geom=spline, orient=False)
+plotter.add_mesh(pc)
+#plotter.add_mesh(pc2)
+#plotter.show(cmap="Reds", scalars="arc_length", show_scalar_bar=False)
+plotter.show()
+
+spline = pyvista.Spline(np.array([[1, 1, 1], [-1, -1, -1]]), 2).tube(radius=0.01)
+spline.plot(scalars='arc_length', show_scalar_bar=False)
